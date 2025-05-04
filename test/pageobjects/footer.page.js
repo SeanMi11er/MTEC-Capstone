70483b5a9956cc11f82d5f9c718c833562b51b11
Sean Miller <sean.miller4303@STU.MTEC.EDU>
2025-05-04 12:34:39 -0600

    Updated POM and spec files to remove expect/logic from spec files

--- test/pageobjects/footer.page.js
+++ test/pageobjects/footer.page.js
@@ -1,42 +1,31 @@
 class FooterPage {
-    get corporateSection() {
-        return $('section#text-2');
-    }
+    // Footer sections and email links
+    get corporateSection() { return $('div.flex_column:first-child'); }
+    get mainOfficeSection() { return $('#text-3'); }
+    get estimatesSection() { return $('#text-5'); }
+    get officeHoursSection() { return $('#text-4'); }
+    
+    get infoEmailLink() { return $('a[href="mailto:info@imsmasonry.com"]'); }
+    get estimatesEmailLink() { return $('a[href="mailto:estimates@imsmasonry.com"]'); }
 
-    get mainOfficeSection() {
-        return $('section#text-3');
-    }
-
-    get estimatesSection() {
-        return $('section#text-5');
-    }
-
-    get officeHoursSection() {
-        return $('section#text-4');
-    }
-
-    get infoEmailLink() {
-        return $('a[href="mailto:info@imsmasonry.com"]');
-    }
-
-    get estimatesEmailLink() {
-        return $('a[href="mailto:estimates@imsmasonry.com"]');
-    }
-
+    // Verify footer sections and details are visible
     async verifyFooterDetailsPresent() {
-        await expect(this.corporateSection).toBeDisplayed();
-        await expect(this.mainOfficeSection).toBeDisplayed();
-        await expect(this.estimatesSection).toBeDisplayed();
-        await expect(this.officeHoursSection).toBeDisplayed();
+        // Wait for footer sections to be displayed
+        await this.corporateSection.waitForDisplayed({ timeout: 5000 });
+        await this.mainOfficeSection.waitForDisplayed({ timeout: 5000 });
+        await this.estimatesSection.waitForDisplayed({ timeout: 5000 });
+        await this.officeHoursSection.waitForDisplayed({ timeout: 5000 });
     }
 
-    async hoverAndClickEmail(link) {
-        await link.scrollIntoView();
-        await link.moveTo();
-        await expect(link).toBeClickable();
-        await link.click();
+    // Hover over and click on email links
+    async hoverAndClickEmail(emailLink) {
+        await emailLink.waitForDisplayed({ timeout: 5000 });
+        await emailLink.moveTo();
+        await emailLink.click();
+        
+        // If you want to check if the mail client opens, additional checks can be added, but this is beyond the scope.
     }
 }
 
-export default new FooterPage();
+module.exports = new FooterPage();
 
